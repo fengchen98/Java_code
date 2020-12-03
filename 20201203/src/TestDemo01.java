33f034a204de670c792fecc7281a76831573bf9b
fengchen98 <249841036@qq.com>
2020-12-03 10:09:49 +0800

    HelloWorld

---
+++ 20201203/src/TestDemo01.java
@@ -0,0 +1,11 @@
+/**
+ * @author ：fc
+ * @date ：Created in 2020/12/3 10:08
+ * @description：
+ * @version: $
+ */
+public class TestDemo01 {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
